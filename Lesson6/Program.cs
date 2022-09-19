2bb5926229a1579f8cc5ed2cc81e435606b57d00
Markov_Yury <umarkov@inbox.ru>
2022-09-19 15:46:56 +0300

    Lesson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main()
+        {
+            short user_input = Convert.ToInt16(Console.ReadLine());
+            switch(user_input)
+            {
+                case 5: Console.WriteLine("Number is 5"); break;
+                case 15: Console.WriteLine("Number is 15"); break;
+                case 20: Console.WriteLine("Number is 20"); break;
+                case 250: Console.WriteLine("Number is 250"); break;
+                default: Console.WriteLine("Number is unrecognized!"); break;
+            }
+        }
+    }
+}
