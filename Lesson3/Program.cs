efff95924b1c040e8ae517cbb785df21bf8fe29a
Markov_Yury <umarkov@inbox.ru>
2022-09-19 06:58:55 +0300

    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main()
+        {
+            // byte minbyte = 0;
+            // Console.WriteLine(minbyte);
+            // byte maxbyte = 255;
+            // Console.WriteLine(maxbyte);
+            // sbyte minsbyte = -128;
+            // Console.WriteLine(minsbyte);
+            // sbyte maxsbyte = 127;
+            // Console.WriteLine(maxsbyte);
+            // short minshort = -32768;
+            // Console.WriteLine(minshort);
+            // short maxshort = 32767;
+            // Console.WriteLine(maxshort);
+            // ushort minushort = 0;
+            // Console.WriteLine(minushort);
+            // ushort maxushort = 65535;
+            // Console.WriteLine(maxushort);
+            // int minint = -2147483648;
+            // Console.WriteLine(minint);
+            // int maxint = 2147483647;
+            // Console.WriteLine(maxint);
+            // uint minuint = 0;
+            // Console.WriteLine(minuint);
+            // uint maxuint = 4294967295;
+            // Console.WriteLine(maxuint);
+            // long minlong = -9223372036854775808;
+            // Console.WriteLine(minlong);
+            // long maxlong = 9223372036854775807;
+            // Console.WriteLine(maxlong);
+            // ulong minulong = 0;
+            // Console.WriteLine(minulong);
+            // ulong maxulong = 18446744073709551615;
+            // Console.WriteLine(maxulong);
+            // float minfloat = -1000004.5345999999f;
+            // Console.WriteLine(minfloat);
+            // float maxfloat = 1000004.5345999999f;
+            // Console.WriteLine(maxfloat);
+            // double mindouble = -17000000000000000000.0000000000000000000000000000000000000000000000001d;
+            // Console.WriteLine(mindouble);
+            // double maxdouble = 17000000000000000000.000000000000000000000000000000000000000000000000001d;
+            // Console.WriteLine(maxdouble);
+            // decimal mindecimal = 0;
+            // Console.WriteLine(mindecimal);
+            // decimal maxdecimal = 255;
+            // Console.WriteLine(maxdecimal);
+            // string word = "Переменная: ";
+            // int number = -10;
+            // char symbol = 's';
+            // bool isHappy = true;
+            // Console.WriteLine(word + number + "." + " " + isHappy + " " + symbol);
+            // Получение данных
+            // Console.ReadKey();
+
+            int num_1 = 0, num_2 = 0;
+            Console.WriteLine(num_1);
+            Console.WriteLine(num_2);
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First: " + num_1 + ". Second: " + num_2);
+            
+             
+        }
+    }
+}
