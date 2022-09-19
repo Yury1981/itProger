961c2bf1cf3666e6b63b44f7bc3bf74994577319
Markov_Yury <umarkov@inbox.ru>
2022-09-19 05:03:44 +0300

    Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello 1\n");
+            // Hello World!
+            /*
+            Огромный
+            комментарий!
+            */
+            // Быстрый комментарий (отмена комментария) Ctrl + /
+            Console.Write("Hello 2\n");
+            Console.Write("Hello 3\n");
+            Console.ReadLine();
+        }
+    }
+}
