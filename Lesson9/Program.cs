61bae9fe1d0d69c1497c537a9a4121424c625bd9
Markov_Yury <umarkov@inbox.ru>
2022-09-21 07:56:05 +0300

    Lesson9

---
+++ Lesson9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson9
+{
+    class Program
+    {
+        static void Main()
+        {
+            // short[] nums = {5, 6, 7, 8, 5, 3, 4};
+            // foreach(short el in nums) Console.WriteLine("El: " + el);
+
+            // short[,] nums = {{5, 6, 7}, {8, 5, 3}, {54, 7, 20}};
+            // foreach(short el in nums) Console.WriteLine("El: " + el);
+
+            List<int> numbers = new List<int>() {4, 6, 7};
+            numbers.Add(40);
+            numbers.Add(100);
+            numbers.Add(5);
+            numbers.Remove(100);
+            numbers.Sort();
+            numbers.Reverse();
+            
+            foreach(int el in numbers) Console.WriteLine("El: " + el);
+        }
+    }
+}
