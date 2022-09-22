41f4f0c529272625be5ccb105dbc37e36e577a02
Markov_Yury <umarkov@inbox.ru>
2022-09-22 15:17:59 +0300

    Lesson12

---
+++ Lesson12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson12
+{
+    class Program
+    {
+        static void Main()
+        {
+            // try
+            // {
+            //     int num = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine(num);
+            // }
+            // catch(FormatException)
+            // {
+            //     Console.WriteLine("Vveli ne tot format!");
+            // }
+
+            try{
+            int a, b, res;
+            Console.Write("Vvedite 1-e chislo: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Vvedite 2-e chislo: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            res = a / b;
+            Console.WriteLine(res);
+            }
+            catch(DivideByZeroException){
+                Console.WriteLine("Delenie na 0!");
+            }
+            catch(FormatException){
+                Console.WriteLine("Vveli ne tot format!");
+            }
+            finally{
+                
+            }
+            
+        }
+    }
+}
