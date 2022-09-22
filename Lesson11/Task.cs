708d98a1f32b89403e24fb6aeff2a7414f3a9d11
Markov_Yury <umarkov@inbox.ru>
2022-09-22 13:57:33 +0300

    Lesson11

---
+++ Lesson11/Task.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Console.WriteLine("Vvedite text: ");
+            // string text = Console.ReadLine();
+            // using(FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+            // {
+            //     byte[] array = System.Text.Encoding.Default.GetBytes(text);
+            //     stream.Write(array, 0, array.Length);
+            // }
+
+            using(FileStream stream1 = File.OpenRead("info.txt"))
+            {
+                byte[] array = new byte[stream1.Length];
+                stream1.Read(array, 0, array.Length);
+
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine(textFromFile);
+            }
+        }
+    }
+}
